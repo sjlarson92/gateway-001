07d81f085fd0e02386a63edf2ecc869766950533
Sasha Larson <sjlarson92@gmail.com>
2020-02-27 21:13:31 -0600

    created gateway and updated routes accordingly

---
+++ src/main/java/com/sasha/gateway001/CommonConfig.java
@@ -0,0 +1,27 @@
+package com.sasha.gateway001;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CommonConfig {
+
+    @Bean
+    public Filter filter() {
+        return new Filter();
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST");
+            }
+        };
+    }
+}
