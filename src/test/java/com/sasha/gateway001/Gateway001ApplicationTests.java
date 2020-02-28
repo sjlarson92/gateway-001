cf57ba3a32ffd8606530ccdacfe417bdd03482f4
Sasha Larson <sjlarson92@gmail.com>
2020-02-27 19:46:17 -0600

    initial commit

---
+++ src/test/java/com/sasha/gateway001/Gateway001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sasha.gateway001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gateway001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
