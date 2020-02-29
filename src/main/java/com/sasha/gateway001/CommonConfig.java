b5534a1c132f51eb3c538b590a41251b1375e92a
Sasha Larson <sjlarson92@gmail.com>
2020-02-29 16:29:38 -0600

    created environmental variables for local/prod

--- src/main/java/com/sasha/gateway001/CommonConfig.java
+++ src/main/java/com/sasha/gateway001/CommonConfig.java
@@ -1,5 +1,6 @@
 package com.sasha.gateway001;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,6 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CommonConfig {
+
+    @Value("${DAP_APP_URL}")
+    String DAP_APP_URL;
 
     @Bean
     public Filter filter() {
@@ -19,7 +23,7 @@ public class CommonConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(DAP_APP_URL)
                         .allowedMethods("GET", "POST");
             }
         };
